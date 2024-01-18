936663c715715868ff28951c93439149efbcb3e0
Rusty <proga100@gmail.com>
2024-01-18 22:11:41 +0500

    ww

---
+++ assets/js/app-ajax-download.js
@@ -0,0 +1,52 @@
+(function($) {
+    "use strict";
+
+    var stm_single_filter_link = false;
+    var stmIsotope;
+    var $container = $('.stm-isotope-sorting');
+
+    $(document).ready(function () {
+
+    });
+
+function stm_ajax_add_download_pdf() {
+        $('#downloadPDF form').on("submit", function(event){
+            event.preventDefault();
+            $.ajax({
+                url: ajaxurl,
+                type: "POST",
+                dataType: 'json',
+                context: this,
+                data: $( this ).serialize() + '&action=stm_ajax_add_download_pdf',
+                beforeSend: function(){
+                    $('.alert-modal').remove();
+                    $(this).closest('form').find('input').removeClass('form-error');
+					$(this).closest('form').find('.stm-ajax-loader').addClass('loading');
+                },
+                success: function (data) {
+
+			    $(this).closest('form').find('.stm-ajax-loader').removeClass('loading');
+	                    $(this).closest('form').find('.modal-body').append('<div class="alert-modal alert alert-'+ data.status +' text-left">' + data.response + '</div>')
+	                    for(var key in data.errors) {
+	                        $('#request-download-pdf-form input[name="' + key + '"]').addClass('form-error');
+	                    }
+	                    if(data.status == 'success'){
+				    var download_pdf_link = $('#download_pdf_link').val();
+				    //window.location.href = download_pdf_link;
+				    //console.log(download_pdf_link);
+				    if(download_pdf_link !== '') {
+				    	downloadFile(download_pdf_link);
+				    }
+
+	                    }
+                }
+            });
+            $(this).closest('form').find('.form-error').live('hover', function () {
+                $(this).removeClass('form-error');
+            });
+        });
+    }
+
+})(jQuery);
+
+
